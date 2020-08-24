1398d577fa3d6f8a45c8e3a4d9f1e08dec3be6b8
Tom Gurion <nagasaki45@gmail.com>
2020-08-24 14:19:49 +0100

    Minimal working example

--- server.py
+++ server.py
@@ -2,14 +2,31 @@
 Decrypto party game, online multiplayer version. Based on the work of
 WhoaWhoa.
 '''
-from flask import Flask, render_template
+import random
+import string
 
-app = Flask(__name__)
-teams = {}  # team hash -> (starting_time, words)
+import flask
+
+app = flask.Flask(__name__)
+teams = {}  # team hash -> words
 with open('wordlist.txt') as f:
-    words = f.read().splitlines()
+    wordlist = f.read().splitlines()
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return flask.render_template('index.html')
+
+
+@app.route('/new-team')
+def new_team():
+    team = ''.join(random.sample(string.ascii_uppercase + string.digits, 6))
+    words = random.sample(wordlist, 4)
+    teams[team] = words
+    return {'team': team, 'words': words}
+
+
+@app.route('/get-team')
+def get_team():
+    team = flask.request.args.get("team")
+    return {'words': teams[team]}
--- static/decrypto.js
+++ static/decrypto.js
@@ -1,130 +1,55 @@
-var wordList;
-
-function loadWordList() {
-	var wordList;
-
-	$.ajax({
-            url : "wordlist.txt",
-            dataType: "text",
-            success : function (data) {
-            	wordList = data.split("\n");
-            },
-            async: false
-        });
-	return wordList;
-}
-
-function pickWords(numWords) {
-	return _.sample(wordList, numWords);
-}
-
 function generateCode() {
 	var code = _.sample([1,2,3,4], 3);
-	Cookies.set("code", code);
 	setCode(code);
-	$('#codeModal').modal('show');
 }
 
 function setCode(code) {
-	for(idx = 0; idx < 3; idx++) {
-		$('#code' + idx).text(code[idx]);
-	}
-	$('#revealCodeButton').show();
+    for(idx = 0; idx < 3; idx++) {
+        $('#code' + idx).text(code[idx]);
+    }
+    $('#codeModal').modal('show');
 }
 
 function setWords(words) {
-	for(idx = 0; idx < 4; idx++) {
-		$('#word' + idx).text(words[idx]);
-	}
-}
-
-function loadWords() {
-	return Cookies.getJSON("words");
-}
-
-function loadCode() {
-	var code = Cookies.getJSON("code");
-	if (code) {
-		setCode(code);
-	} else {
-		$('#revealCodeButton').hide();
-	}
-}
-
-function newGame() {
-	var words = pickWords(4);
-	Cookies.set("words", words);
-	Cookies.remove("code");
-	$('#revealCodeButton').hide();
-	return words;
-}
-
-function toggleFullScreen() {
-	if (screenfull.isFullscreen) {
-		screenfull.exit();
-	} else {
-		screenfull.request();
-	}
-	setFullScreenIcon();
-}
-
-function setFullScreenIcon() {
-    if (screenfull.isFullscreen) {
-		$('#enableFullScreen').hide();
-		$('#disableFullScreen').show();
-    } else {
-		$('#enableFullScreen').show();
-		$('#disableFullScreen').hide();
+    for(idx = 0; idx < 4; idx++) {
+        $('#word' + idx).text(words[idx]);
     }
 }
 
-function startNewGame() {
-	setWords(newGame());
+function newTeam() {
+    var newTeamData;
+    $.ajax({
+        url: "new-team",
+        success: function(data) {
+            newTeamData = data;
+        },
+        async: false
+    });
+    var words = newTeamData["words"];
+    setWords(words);
+    window.location.hash = newTeamData["team"];
 }
 
-function disableScreenLock() {
-	var noSleep = new NoSleep();
-	noSleep.enable();
-}
-
-function initScreenfull() {
-	var noSleep = new NoSleep();
-
-	if (screenfull.enabled) {
-		screenfull.on('change', () => {
-			if (screenfull.isFullscreen) {
-				noSleep.enable();
-			} else {
-				noSleep.disable();
-			}
-		});
-        setInterval(setFullScreenIcon, 200);
-    } else {
-        $('#fullScreenButton').hide();
-        $('#disableScreenLockModal').modal('show');
-    }
+function openTeam(team) {
+    var words;
+    $.ajax({
+        url: "get-team",
+        data: {team: team},
+        success: function(data) {
+            words = data["words"];
+            console.log(data);
+        },
+        async: false
+    });
+    setWords(words);
 }
 
 function initialize() {
-	initScreenfull();
+    var team = window.location.hash;
 
-	wordList = loadWordList();
-	loadCode();
-
-	var words = loadWords();
-
-	if (words) {
-		setWords(words);
-	} else {
-		/*
-		startNewGame();
-		$('#newGameModalCancelButton').hide();
-		$('#newGameModal').modal({
-			show: true,
-			keyboard: false,
-			backdrop: 'static'
-		});
-		*/
-		$('#newGameModal').modal('show');
-	}
+    if (team) {
+        openTeam(team.replace("#", ""));
+    } else {
+        newTeam();
+    }
 }
