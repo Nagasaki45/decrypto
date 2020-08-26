f7e85c5cbe198e230f30762484494c2bcb650c0a
Tom Gurion <nagasaki45@gmail.com>
2020-08-26 10:55:28 +0100

    Store teams on redis. Fix #1

--- server.py
+++ server.py
@@ -2,13 +2,18 @@
 Decrypto party game, online multiplayer version. Based on the work of
 WhoaWhoa.
 '''
+import json
+import os
 import random
 import string
 
 import flask
+import redis
+
+DEFAULT_REDIS_URL = 'redis://localhost:6379/0'
 
 app = flask.Flask(__name__)
-teams = {}  # team hash -> words
+r = redis.from_url(os.environ.get('REDIS_URL', DEFAULT_REDIS_URL))
 with open('wordlist.txt') as f:
     wordlist = f.read().splitlines()
 
@@ -17,13 +22,14 @@ with open('wordlist.txt') as f:
 def index():
     team = ''.join(random.sample(string.ascii_uppercase + string.digits, 6))
     words = random.sample(wordlist, 4)
-    teams[team] = words
+    r.set(team, json.dumps(words))
     return flask.redirect(flask.url_for('team', team=team))
 
 
 @app.route('/<team>')
 def team(team):
-    words = teams.get(team)
+    words = r.get(team)
     if words:
+        words = json.loads(words)
         return flask.render_template('index.html', words=words)
     return flask.render_template('404.html'), 404
