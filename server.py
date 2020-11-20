62e4f523d6f7a2b89da67efa09082d37126d9085
Tom Gurion <nagasaki45@gmail.com>
2020-11-20 16:06:34 +0000

    Hebrew support

--- server.py
+++ server.py
@@ -5,24 +5,34 @@ WhoaWhoa.
 import json
 import os
 import random
+import pathlib
 import string
 
 import flask
 import redis
 
+WORDLISTS_DIR = pathlib.Path('wordlists')
 DEFAULT_REDIS_URL = 'redis://localhost:6379/0'
 EXPIRE = 60 * 60 * 24 * 30  # 30 days
+LANGUAGES = {
+    'eng': 'English',
+    'heb': 'Hebrew',
+}
 
 app = flask.Flask(__name__)
 r = redis.from_url(os.environ.get('REDIS_URL', DEFAULT_REDIS_URL))
-with open('wordlist.txt') as f:
-    wordlist = f.read().splitlines()
+
+wordlists = {}
+for language in LANGUAGES:
+    with open(WORDLISTS_DIR / language) as f:
+        wordlists[language] = f.read().splitlines()
 
 
 @app.route('/')
 def index():
+    language = flask.request.args.get('language', 'eng')
     team = ''.join(random.sample(string.ascii_uppercase + string.digits, 6))
-    words = random.sample(wordlist, 4)
+    words = random.sample(wordlists[language], 4)
     r.set(team, json.dumps(words), ex=EXPIRE)
     return flask.redirect(flask.url_for('team', team=team))
 
@@ -32,5 +42,9 @@ def team(team):
     words = r.get(team)
     if words:
         words = json.loads(words)
-        return flask.render_template('index.html', words=words)
+        return flask.render_template(
+            'index.html',
+            words=words,
+            languages=LANGUAGES,
+        )
     return flask.render_template('404.html'), 404
